77defb4546d9d35a8775717aca17fa4e7c067343
ZWkang <907747874@qq.com>
2016-08-07 23:23:36 +0800

    update

--- admin/models/Group_model.php
+++ admin/models/Group_model.php
@@ -5,30 +5,46 @@ class Group_model extends CI_Model{
 		parent::__construct();
 		$this->load->database();
 	}
+	/**
+	 * 得到所有用户组
+	 */
 	public function GetAllGroup(){
 		$this->db->order_by('gid','acs');
 		$result = $this->db->get($this->table);
 		return $result->result_array();
 	}
+	/**
+	 * 得到一个用户组信息
+	 */
 	public function GetOneGroup($group_id){
 		$this->db->where('gid',$group_id);
 		return $this->db->get($this->table)->row_array();
 	}
+	/**
+	 * 增加用户组
+	 */
 	public function AddGroup($data){
 		$this->db->insert($this->table,$data);
 		return $this->db->affected_rows();
 	}
+	/**
+	 * 修改用户组
+	 */
 	public function EditGroup($id,$data){
 		$this->db->where('gid',$id);
 		$this->db->update($this->table,$data);
 		return $this->db->affected_rows();
 	}
+	/**
+	 * 删除用户组
+	 */
 	public function DelGroup($id){
 		$id=$id+0;
 		$this->db->where('gid',$id);
 		$this->db->delete($this->table);
 		return $this->db->affected_rows();
 	}
+	//得到权限
 	public function getwithpression($arr){
 		$this->db->where_in('id',$arr);
 		$result = $this->db->get('blog_premission');
